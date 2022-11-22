e1530f93c892fd7286e5407037d7d1adb40cf08a
Miliyonayalew <miliayalew@gmail.com>
2022-11-22 18:07:37 +0300

    Create Compnents

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => (
+  <div>
+    <Navbar title="Crypto App" />
+    <h1>Home</h1>
+  </div>
+);
+
+export default Home;
