de7d2a0e6f4d68cc37ba3c86e959b7f6c4c5a750
Miliyonayalew <miliayalew@gmail.com>
2022-11-23 14:39:12 +0300

    Add path id for route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/details" element={<Details />} />
+        <Route path="/:id" element={<Details />} />
       </Routes>
     </BrowserRouter>
   );
